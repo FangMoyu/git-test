2d0ece3a73209f0d3785197e145e78c8f73658fa
Shirly <107528339+FangMoyu@users.noreply.github.com>
2023-07-02 15:55:36 +0800

    Update GitTest.java

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master Git!");
         System.out.println("hot-fix Git!");
         System.out.println("push gitHub test!");
+        System.out.println("pull gitHub test!");
     }
 }
